fe534ef97c007093170b788e7936fdce7d88fbeb
safekhawaja <68385788+safekhawaja@users.noreply.github.com>
2020-08-12 16:57:46 -0400

    initial push

---
+++ Assets/Scripts/AddJump.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AddJump : MonoBehaviour
+{
+
+    public float fallMultiplier = 2.5f;
+    public float lowJumpMultiplier = 2f;
+    public float jumpspeed = 2f;
+
+    Rigidbody rb;
+
+    void Awake(){
+       rb = GetComponent<Rigidbody>();
+    }
+    void FixedUpdate(){
+         if (Input.GetButtonDown("Jump")) { rb.velocity += Vector3.up * jumpspeed; };
+
+         if (rb.velocity.y < 0) {
+            rb.velocity += Vector3.up * Physics.gravity.y * (fallMultiplier - 1) * Time.deltaTime;
+        } 
+        else if (rb.velocity.y > 0 && !Input.GetButton("Jump")) {
+            rb.velocity += Vector3.up * Physics.gravity.y * (lowJumpMultiplier - 1) * Time.deltaTime;
+        }
+    }
+}
---
+++ Assets/Scripts/AddPlayerControlledVellocity.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AddPlayerControlledVellocity : MonoBehaviour
+{
+
+    [SerializeField]
+    Vector3 v3Force;
+
+    [SerializeField]
+    KeyCode keyPositive;
+    [SerializeField]
+    KeyCode keyNegative;
+
+    void FixedUpdate()
+    {
+        if (Input.GetKey(keyPositive))
+            gameObject.GetComponent<Rigidbody>().velocity += v3Force; 
+
+        if (Input.GetKey(keyNegative))
+            gameObject.GetComponent<Rigidbody>().velocity -= v3Force;
+    }
+}
